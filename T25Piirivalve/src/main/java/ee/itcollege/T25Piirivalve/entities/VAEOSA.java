f7cec2098cef6ebdb2775bffc426c181b7553038
Janno Tomingas <janno.tomingas@gmail.com>
2011-11-30 12:21:52 +0200

    + üksus ja väeosa

---
+++ T25Piirivalve/src/main/java/ee/itcollege/T25Piirivalve/entities/VAEOSA.java
@@ -0,0 +1,124 @@
+package ee.itcollege.T25Piirivalve.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: VAEOSA
+ *
+ */
+@Entity
+@RooToString
+@RooEntity
+
+public class VAEOSA implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long vaeosa_ID;  
+	private String avaja;
+	private Date avatud;
+	private String muutja;
+	private Date muudetud;
+	private String sulgeja;
+	private Date suletud;
+	private String kood;
+	private String nimetus;
+	private String kommentaar;
+	private Date alates;
+	private Date kuni;
+	private static final long serialVersionUID = 1L;
+
+	public VAEOSA() {
+		super();
+	}
+	
+	public Long getVaeosa_ID() {
+		return this.vaeosa_ID;
+	}
+
+	public void setRiigi_admin_yksus_ID(Long riigi_admin_yksus_ID) {
+		this.riigi_admin_yksus_ID = riigi_admin_yksus_ID;
+	} 
+	
+	public String getAvaja() {
+		return this.avaja;
+	}
+
+	public void setAvaja(String avaja) {
+		this.avaja = avaja;
+	}   
+	public Date getAvatud() {
+		return this.avatud;
+	}
+
+	public void setAvatud(Date avatud) {
+		this.avatud = avatud;
+	}   
+	public String getMuutja() {
+		return this.muutja;
+	}
+
+	public void setMuutja(String muutja) {
+		this.muutja = muutja;
+	}   
+	public Date getMuudetud() {
+		return this.muudetud;
+	}
+
+	public void setMuudetud(Date muudetud) {
+		this.muudetud = muudetud;
+	}   
+	public String getSulgeja() {
+		return this.sulgeja;
+	}
+
+	public void setSulgeja(String sulgeja) {
+		this.sulgeja = sulgeja;
+	}   
+	public Date getSuletud() {
+		return this.suletud;
+	}
+
+	public void setSuletud(Date suletud) {
+		this.suletud = suletud;
+	}   
+	public String getKood() {
+		return this.kood;
+	}
+
+	public void setKood(String kood) {
+		this.kood = kood;
+	}   
+	public String getNimetus() {
+		return this.nimetus;
+	}
+
+	public void setNimetus(String nimetus) {
+		this.nimetus = nimetus;
+	}   
+	public String getKommentaar() {
+		return this.kommentaar;
+	}
+
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}   
+	public Date getAlates() {
+		return this.alates;
+	}
+
+	public void setAlates(Date alates) {
+		this.alates = alates;
+	}   
+	public Date getKuni() {
+		return this.kuni;
+	}
+
+	public void setKuni(Date kuni) {
+		this.kuni = kuni;
+	}
+   
+}
