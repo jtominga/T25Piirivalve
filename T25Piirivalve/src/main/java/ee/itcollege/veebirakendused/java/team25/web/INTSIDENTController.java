df31c70e496c9540f26820cede2e4ba1d827a934
Tanel Lebedev <tanel.lebedev@gmail.com>
2011-12-18 14:35:49 +0200

    Kasutamata impordid maha

--- T25Piirivalve/src/main/java/ee/itcollege/veebirakendused/java/team25/web/INTSIDENTController.java
+++ T25Piirivalve/src/main/java/ee/itcollege/veebirakendused/java/team25/web/INTSIDENTController.java
@@ -1,30 +1,21 @@
 package ee.itcollege.veebirakendused.java.team25.web;
 
-import ee.itcollege.T25Piirivalve.entities.INTSIDENDI_LIIK;
-import ee.itcollege.T25Piirivalve.entities.INTSIDENT;
-import ee.itcollege.T25Piirivalve.entities.PIIRILOIK;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import java.io.UnsupportedEncodingException;
-import java.lang.Integer;
-import java.lang.Long;
-import java.lang.String;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriUtils;
-import org.springframework.web.util.WebUtils;
+
+import ee.itcollege.T25Piirivalve.entities.INTSIDENDI_LIIK;
+import ee.itcollege.T25Piirivalve.entities.INTSIDENT;
+import ee.itcollege.T25Piirivalve.entities.PIIRILOIK;
 
 @RooWebScaffold(path = "intsidents", formBackingObject = INTSIDENT.class)
 @RequestMapping("/intsidents")
